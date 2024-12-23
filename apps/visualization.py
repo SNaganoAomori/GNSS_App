bdbac894b95a54ea9f7e63a838d9a8583c90ab89
SNaganoAomori <99630319+SNaganoAomori@users.noreply.github.com>
2024-12-23 16:29:59 +0900

    [Update] change marker size & line width

--- apps/visualization.py
+++ apps/visualization.py
@@ -21,28 +21,28 @@ def visualize_data(df: pl.DataFrame, time_series: bool=False) -> go.Figure:
         fig.add_trace(
             go.Scatter(
                 x=df[jn_confs.datetime_col], y=df[jn_confs.epochs_col],
-                mode='markers+lines', marker_size=3
+                mode='markers+lines', marker_size=5
             ), row=1, col=1
         )
         #------------------ pdop ------------------#
         fig.add_trace(
             go.Scatter(
                 x=df[jn_confs.datetime_col], y=df[jn_confs.pdop_col],
-                mode='markers+lines', marker_size=3
+                mode='markers+lines', marker_size=5
             ), row=1, col=2
         )
         #------------------ n-satellites ------------------#
         fig.add_trace(
             go.Scatter(
                 x=df[jn_confs.datetime_col], y=df[jn_confs.satellites_col],
-                mode='markers+lines', marker_size=3
+                mode='markers+lines', marker_size=5
             ), row=2, col=1
         )
         #------------------ holizontal std ------------------#
         fig.add_trace(
             go.Scatter(
                 x=df[jn_confs.datetime_col], y=df[jn_confs.hstd_col],
-                mode='markers+lines', marker_size=3
+                mode='markers+lines', marker_size=5
             ), row=2, col=2
         )
             
@@ -73,7 +73,7 @@ def visualize_data(df: pl.DataFrame, time_series: bool=False) -> go.Figure:
     fig.update_traces(
         marker_color='#008899',
         marker_line_color='black',
-        marker_line_width=2,
+        marker_line_width=0.5,
         opacity=0.75,
     )
     return fig
